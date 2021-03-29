d70b2d738ed4f4802583bf2687f1cf50ba763ecc
Arvid Carlander <arvid@carlander.ac>
2021-03-29 22:11:23 +0200

    x

--- general/routes/index.js
+++ general/routes/index.js
@@ -2,6 +2,7 @@ const {exec} = require("child_process");
 var express = require('express');
 var router = express.Router();
 var path = require('path')
+//var Math = require('Math')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +17,17 @@ router.get('/', function(req, res, next) {
 	res.sendFile(path.join(__dirname, '../html', 'index.html'));
 });
 
+// Test of chart.js for Tfv
+router.get('/charttest', function(req, res, next) {
+  //res.render('index', { title: 'chart.js' });
+
+    	if (req.query.name){
+        	console.log(`Chart test: Requesting ${req.query.name}`)
+    	}
+    	console.log("Returning the static charttest.htm")
+	res.sendFile(path.join(__dirname, '../html', 'charttest.html'));
+});
+
 router.get('/public/javascripts/client.js', function(req, res, next) {
     	//res.sendFile('/home/pi/nodeprojects/general/public/javascripts/client.html')
 	res.sendFile(path.join(__dirname, '../public/javascripts', 'client.html'));
@@ -26,7 +38,7 @@ router.get('/public/javascripts/client.js', function(req, res, next) {
 
 /*  */
 router.get('/api', function(req, res, next) {
-  		res.send( 'Try <a href="api/number/temp/w1/tegel "> api/number/temp/w1/tegel </a>')
+  		res.send( 'Try <a href="api/number/temp/w1/Duckling "> api/number/temp/w1/Duckling </a>')
 });
 
 router.get('/api/raw/temp/w1', function(req, res, next) {
@@ -38,9 +50,14 @@ router.get('/api/raw/temp/w1', function(req, res, next) {
 });
 
 function w1temptostring(strinp) {
-	//let txt = strinp.split('=')[2] 
 	
-	return ((strinp.split('=')[2] / 1000) + "")
+	//return ((strinp.split('=')[2] / 1000) + "")
+	return (w1temp(strinp) + "")
+}
+
+function w1temp(strinp) {
+	
+	return (Math.round(strinp.split('=')[2] / 100) / 10)
 }
 
 router.get('/api/:fmt/temp/w1/tegel', function(req, res, next) {
@@ -62,6 +79,32 @@ router.get('/api/:fmt/temp/w1/tegel', function(req, res, next) {
 		
 		}
 	})
+});
+
+let getDuck = function(req, res, next) {
+	exec( 'cat /sys/bus/w1/devices/28-0301a279df82/w1_slave'
+		,(error,stdout,stderr) => {
+		let t = w1temptostring(stdout)
+		switch (req.params.fmt) {
+			case "raw":
+				res.send(stdout)
+				break
+			case "json":
+				res.json( { "name" : "Duckling", "temp" : t})
+				break
+			//case "number":
+			default:
+				res.send(t)
+				break
+		
+		}
+	})
+}
+router.get('/api/:fmt/temp/w1/Duckling', getDuck);
+
+router.get('/Duckling', function(req, res, next) {
+
+	res.sendFile(path.join(__dirname, '../html', 'ducklingTemp.html'));
 });
 
 router.get('/api/:fmt/temp/cpu', function(req, res, next) {
