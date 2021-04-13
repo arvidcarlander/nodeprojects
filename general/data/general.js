2f5aabc9b347fcc17777fb7a246c3ff436a23ae9
Arvid Carlander <arvid@carlander.ac>
2021-04-13 21:19:05 +0200

    works with better json

--- general/data/general.js
+++ general/data/general.js
@@ -1,10 +1,49 @@
-//let j = require("./general.json")
-let dataUtils = require("./datautils.js")
-console.log("dataUtils.data: ")
-console.dir(dataUtils.data)
+// Utilities for handling the data in general.json
 
-console.log("dataUtils.data.by28: ")
-console.dir(dataUtils.data.by28)
+var sourceData=require('./general.json')
+var debug = false
 
-console.log("dataUtils.data.byname: ")
-console.dir(dataUtils.data.byname)
+
+
+
+var by28 = {}
+for (const host in sourceData.w1) {
+	debug && console.log(`host is: ${host}`)
+	for (const name in sourceData.w1[host]) {
+		debug && console.log(`name is: ${name}`)
+		//console.log(by28[sourceData.w1[host][name].device)
+		var this28=sourceData.w1[host][name]["device"]
+		debug && console.log("this28 is: " + this28)
+		by28[this28] = name
+	
+	}
+}
+debug &&console.log("by28 is: ")
+debug &&console.dir(by28)
+
+var byName = []
+for (const type in sourceData) {
+	for (const host in sourceData[type]) {
+		debug && console.log(`host is: ${host}`)
+		for (const name in sourceData[type][host]) {
+			debug && console.log(`name is: ${name}`)
+			//console.log(byName[sourceData[type][host][name].device)
+			var device=sourceData[type][host][name]["device"]
+			debug && console.log("device is: " + device)
+			byName.push({
+				name: 	name,
+				device:	device,
+				type:	type,
+				host:	host
+
+		
+			})
+		}
+	}
+}
+
+
+
+exports.sourceData=sourceData
+exports.by28=by28
+exports.byName=byName
---
+++ general/data/generaltest.js
@@ -0,0 +1,11 @@
+let general = require("./general.js")
+
+//console.log("general.sourceData: ")
+//console.dir(general.sourceData)
+
+//console.log("general.by28: ")
+//console.dir(general.by28)
+
+console.log("general.byName: ")
+console.dir(general.byName)
+
