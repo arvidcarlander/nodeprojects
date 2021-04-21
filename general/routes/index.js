4454041e25bdce90a1a9164bddb5b45e9ef7b380
Arvid Carlander <arvid@carlander.ac>
2021-04-21 17:26:07 +0200

    works

--- general/routes/index.js
+++ general/routes/index.js
@@ -4,25 +4,33 @@ var router = express.Router();
 var path = require('path')
 var telldus = require("../utils/telldus.js")
 //var Math = require('Math')
+var debug = false
+// Max age for Telldus info in seconds
+var maxAgeTelldus = 5 * 60 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.sendFile(path.join(__dirname, '../html', 'index.html'));
 });
+// For top-level requests, simply try sending an html  file with the same name as the request. Works for http://xxx/ducklings if there is a page htmp/ducklings/html
+router.get('/:requestName', function(req, res, next) {
+
+	let requestName = req.params.requestName
+	res.sendFile(path.join(__dirname, '../html', requestName + '.html'));
+});
 
 // All Telldus items
 // TODO: 
-//	Check with db whether the device matches w1 or telldus (or oled?) name or id and call relevant function
+//	Combine W1 and Telldus based on type in database. Check with db whether the device matches w1 or telldus (or oled?) name or id and call relevant function
 //	Remove /telldus/
-//	Switch order temp/format
+//	Switch order to temp/format instead of format/temp
 //	Check age on Telldus stuff. Return 0.0 if older than 5 mins
-//	Provide reasonable errors to client if Telldus, 1, or OLED do not respond
+//	Provide reasonable errors to client if Telldus, W1, or OLED does not respond
 //	Teach clients to handle those errors
 //	Standardize temp number to one decimal
 //	Add timestamp to standard format JSON
 router.get('/api/:fmt/temp/telldus/:item', function(req, res, next) {
-
 	switch (req.params.item) {
 		case "outside": 		
 		case "outdoors": 		
@@ -96,12 +104,6 @@ router.get('/api/:fmt/temp/vitavillan', function(req, res, next) {
 });
 
 
-// For top-level requests, simply try sending an html  file with the same name as the request. Works for http://xxx/ducklings if there is a page htmp/ducklings/html
-router.get('/:requestName', function(req, res, next) {
-
-	let requestName = req.params.requestName
-	res.sendFile(path.join(__dirname, '../html', requestName + '.html'));
-});
 
 router.get('/api/:fmt/temp/cpu/:server', function(req,res,next) {
 	getCpu(req, res, next)
@@ -147,15 +149,27 @@ function w1temp(strinp) {
 }
 
 
+// Take a string and format it to two digits before and one after the .
+
 function setNumberFormat(nr) {
-	//if (/./.test(nr)) {
-		//nr = "000" + nr + "000"
-		//let a = nr.split(".")
-		//return(a[1]substring()
-	//} else {
-		return(nr)
-	//}
+        console.log("it is now: " + nr)
+        if ( ! /\./.test(nr)) {
+                console.log("has no .")
+                nr = nr + ".0"
+                console.log("it is now: " + nr)
+        }
+        nr = "000" + nr + "000"
+        console.log("it is now: " + nr)
+        let a = nr.split(".")
+        console.log("a0 is now: " + a[0])
+        console.log("a1 is now: " + a[1])
+        let left  = a[0].substring(3,12)
+        let right = a[1].substring(0,1)
+        console.log("left  is now: " + left )
+        console.log("right  is now: " + right )
+        return(left + "." + right)
 }
+
 
 function getTelldus(id,req,res) {
 	const fubar = telldus.api.getSensorInfo(id)
@@ -163,24 +177,42 @@ function getTelldus(id,req,res) {
 			//sensorInfo.JSON = JSON.parse(sensorInfo)
 			//res.send(sensorInfo.JSON.id)
 			//res.send(sensorInfo.data[0].value)
-			console.dir(sensorInfo)
+			debug && console.dir(sensorInfo)
 
+			let temp = "0"
 			if (  sensorInfo) {
 				allJSON=sensorInfo.data[0]
 				name=sensorInfo.name
 				temp=sensorInfo.data[0].value
 			} else {
 				console.log("Error: Telldus returned nothing. Unknown device?")
-				temp = 0
 				name = "unknown"
 				allJSON={}
 			}
 			if(sensorInfo.data[0].name != "temp") {
 				console.log("Error: Telldus returned no temp. Humidity?")
-				temp = 0
+				temp = "0"
 				name = "unknown"
 				allJSON={}
 			}
+
+			let now = new Date()
+			let nowTimeStamp = now.getTime() / 1000
+			let telldusLastUpdated =  parseFloat(sensorInfo.lastUpdated) 
+			let ageSeconds = nowTimeStamp - telldusLastUpdated
+			if(ageSeconds >= maxAgeTelldus) {
+				debug && console.log("Too old: age is: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" + ageSeconds)
+				debug && console.log("NowTimestamp is: " + nowTimeStamp)
+				debug && console.log("telldusLastUpdated is: " + telldusLastUpdated)
+				debug && console.log("Age in seconds is: " + ageSeconds)
+				debug && console.log("MaxAgeTelldus is: " + maxAgeTelldus)
+				temp = "-"
+				allJSON.value="-"
+			}else{
+				debug && console.log("Not too old: age is: " + ageSeconds)
+			}
+
+
 
 			temp=setNumberFormat(temp)
 
