9b23185d8aa3962f3961c2aa54ac1acc9fe09ad1
arvidcarlander <arvid@carlander.ac>
2021-04-22 19:36:15 +0200

    oledstart

--- general/routes/oled.js
+++ general/routes/oled.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var path = require('path')
 var telldus = require("../utils/telldus.js")
 var oled = require("../utils/oled.js")
+var oledupdater = require("../utils/oledupdater.js")
 var data = require("../data/general.js")
 var serverName = require('os').hostname()
 //var Math = require('Math')
@@ -16,6 +17,16 @@ var maxAgeTelldus = 10 * 60
 router.get('/writeline/:text', function(req, res, next) {
 		
 		oled.writeLine(req.params.text)
+})
+
+router.get('/start', function(req, res, next) {
+		
+		oledupdater.start()
+})
+
+router.get('/stop', function(req, res, next) {
+		
+		oledupdater.stop()
 })
 
 module.exports = router;
