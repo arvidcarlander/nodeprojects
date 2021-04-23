171267b3acd4c0d740a72bce932909411fffd545
Arvid Carlander <arvid@carlander.ac>
2021-04-23 09:52:01 +0200

    Temp fix disable oled for raspberrypi

--- general/app.js
+++ general/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
-var oledRouter = require('./routes/oled');
+//var oledRouter = require('./routes/oled');
 
 var app = express();
 
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/oled', oledRouter);
+//app.use('/api/oled', oledRouter);
 app.use('/api', apiRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
